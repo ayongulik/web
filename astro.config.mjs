61402008cca5cf1844746a6395585e9c0a3ef7b2
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2024-02-29 11:10:57 +0100

    fix: display mermaid graph and alert using mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: "github-light",
-    },
+      theme: "github-light"
+    }
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()]
 });
