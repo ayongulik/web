ea01e4009162cdfb569ea4d011dda2cab5c70fa4
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2024-02-29 22:16:25 +0100

    fix(build): skip image optimization using sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   markdown: {
     shikiConfig: {
-      theme: "github-light"
-    }
+      theme: "github-light",
+    },
   },
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
 });
