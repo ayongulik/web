9eba689ae2c57065e71a4b1222a7593f104f9904
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2024-03-01 11:01:29 +0100

    fix: refactor all link using base path

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { SITE_URL, BASE_PATH } from "./src/config";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_URL,
+  base: BASE_PATH,
   image: {
     service: passthroughImageService(),
   },
---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const SITE_URL = "https://rahmanda.github.io";
+export const BASE_PATH = "/ayongulik-web";
