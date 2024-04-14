7c94bdc9abd7a595154665b92e0002fa2499c23d
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2024-04-15 00:31:08 +0200

    chore: add initial draft for web dev track

--- src/config.ts
+++ src/config.ts
@@ -49,4 +49,62 @@ export const LEARNING_TRACKS = {
       ],
     },
   ],
+  "web-dev": [
+    {
+      level: 0,
+      courses: [
+        {
+          title: "HTML, CSS & Javascript Dasar",
+          path: "",
+          available: false,
+          primary_course: true,
+        },
+      ],
+    },
+    {
+      level: 1,
+      courses: [
+        {
+          title: "Pembuatan Web Service dengan Node.js",
+          path: "",
+          available: false,
+          primary_course: true,
+        },
+        {
+          title: "Relational Database & SQL",
+          path: "",
+          available: false,
+          primary_course: false,
+        },
+      ],
+    },
+    {
+      level: 2,
+      courses: [
+        {
+          title: "Implementasi UI dengan React.js",
+          path: "",
+          available: false,
+          primary_course: false,
+        },
+        {
+          title: "Typescript Dasar",
+          path: "",
+          available: false,
+          primary_course: false,
+        },
+      ],
+    },
+    {
+      level: 3,
+      courses: [
+        {
+          title: "Static Site Generator dengan Astro.js",
+          path: "",
+          available: false,
+          primary_course: false,
+        },
+      ],
+    },
+  ],
 };
