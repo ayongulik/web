6b15a4b900151691ffaa1630bf98a4eaee98343a
Rakun <rakun.ayongulik@gmail.com>
2024-04-17 12:04:18 +0200

    feat: add discussion forum links on homepage

--- src/scripts/link.ts
+++ src/scripts/link.ts
@@ -2,6 +2,9 @@ const linkSelector = "[data-link-search]";
 const buttonShareId = "button-share";
 const popupShareId = "popup-share";
 
+const buttonTanyaId = "button-tanya";
+const popupTanyaId = "popup-tanya";
+
 initLink();
 
 function initLink() {
@@ -18,22 +21,29 @@ function initLink() {
     popupShare?.classList.add("hidden");
   });
 
-  window.addEventListener("click", (e) => {
-    const buttonShare = document.getElementById(buttonShareId);
-    const popupShare = document.getElementById(popupShareId);
+  window.addEventListener("click", dropdownClick(buttonShareId, popupShareId));
+  window.addEventListener("click", dropdownClick(buttonTanyaId, popupTanyaId));
+}
+
+function dropdownClick(buttonDropdownId: string, dropdownId: string) {
+  return (e: Event) => {
+    const button = document.getElementById(buttonDropdownId);
+    const popup = document.getElementById(dropdownId);
     const target = e.target as HTMLElement;
 
-    const isButtonClicked = buttonShare?.contains(target);
+    if (button && target) {
+      const isButtonClicked = button?.contains(target);
 
-    if (isButtonClicked) {
-      popupShare?.classList.toggle("hidden");
-    } else {
-      const popupActive = !popupShare?.classList.contains("hidden");
-      const isClickOutside = !popupShare?.contains(target);
+      if (isButtonClicked) {
+        popup?.classList.toggle("hidden");
+      } else {
+        const popupActive = !popup?.classList.contains("hidden");
+        const isClickOutside = !popup?.contains(target);
 
-      if (popupActive && isClickOutside) {
-        popupShare?.classList.add("hidden");
+        if (popupActive && isClickOutside) {
+          popup?.classList.add("hidden");
+        }
       }
     }
-  });
+  };
 }
