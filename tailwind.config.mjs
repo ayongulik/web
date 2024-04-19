d2f1fe2195faf715cbf8b2ae1f60ef0d4be1b0fb
Rakun <rakun.ayongulik@gmail.com>
2024-04-19 15:18:48 +0200

    fix: moar design changes, especially typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    fontFamily: {
+      heading: ["Spline Sans", "Helvetica", "Arial", "sans-serif"],
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
